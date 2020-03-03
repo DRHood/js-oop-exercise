a852a6dcf6ff23dccacf338e743cfeed02f628d7
Darryl Hood <darrylhood@Darryls-MacBook-Air.local>
2020-03-03 01:30:00 -0500

    part 1: created Tamagotchi constructor

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,7 +1,19 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
-
+class Tamagotchi{
+    constructor(){
+      this.foodInTummy = 10;
+      this.restedness = 10;
+      this.health = 10;
+    }
+  
+    cry(){
+      this.foodInTummy--;
+      console.log('current food in tummy: ' + this.foodInTummy);
+      console.log("WAHH!!!");
+    };
+  };
 
 //create new Tamagotchis
 
