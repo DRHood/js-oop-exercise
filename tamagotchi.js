9db15674fd52721012a1b99cfb1479982675f372
Darryl Hood <darryl.r.hood@gmail.com>
2020-03-03 03:11:22 -0500

    part 3: added new methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -16,6 +16,15 @@ class Tamagotchi{
         console.log('current food in tummy: ' + this.foodInTummy);
         console.log("WAHH!!!");
     };
+    puke(){
+        this.foodInTummy--;
+        console.log(this.name + ' current food in tummy: ' + this.foodInTummy);
+        console.log("WAHH!!!");
+    };
+    yawn(){
+        this.restedness--;
+        console.log(this.name + ' has current restedness of: ' + this.restedness);
+    }
 };
 
 //create new Tamagotchis
@@ -24,6 +33,9 @@ var tamagotchi002 = new Tamagotchi();
 
 var tamagotchi003 = new Tamagotchi('Franci','babybat');
 var tamagotchi004 = new Tamagotchi('Donku', 'sylphi');
+
+var tamagotchi005 = new Tamagotchi('Frankie','cacodemon');
+var tamagotchi006 = new Tamagotchi('Izzy', 'imp');
 //test out your Tamagotchies below via console.logs
 console.log(tamagotchi001);
 console.log(tamagotchi002);
@@ -33,3 +45,6 @@ tamagotchi002.cry();
 
 tamagotchi003.cry();
 tamagotchi004.cry();
+
+tamagotchi005.puke();
+tamagotchi006.yawn();
